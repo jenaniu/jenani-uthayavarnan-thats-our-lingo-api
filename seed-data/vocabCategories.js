663a745b2b2e892bdc4c9ab4350ae9b7517a6c9a
Jenani Uthayavarnan <jenaniu@hotmail.com>
2024-09-12 19:50:28 -0400

    add seed data files for vocab

--- seed-data/vocabCategories.js
+++ seed-data/vocabCategories.js
@@ -1,17 +1,32 @@
 export default [
     {
-      id: 1,
       vocab_category: "Food",
       language: "French",
+      level: 1,
     },
     {
-      id: 2,
       vocab_category: "Travel 101",
       language: "French",
+      level: 1,
     },
     {
-      id: 3,
       vocab_category: "Hello & Goodbye",
       language: "French",
+      level: 1
+    },
+    {
+      vocab_category: "Animals",
+      language: "French",
+      level: 2,
+    },
+    {
+      vocab_category: "Numbers 1-10",
+      language: "French",
+      level: 2,
+    },
+    {
+      vocab_category: "Colours",
+      language: "French",
+      level: 2
     },
   ];
