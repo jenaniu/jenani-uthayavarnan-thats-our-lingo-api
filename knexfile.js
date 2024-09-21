6d3d1ac7c2aa15b1218590f4c3412f3bc71ca4d7
Jenani Uthayavarnan <jenaniu@hotmail.com>
2024-09-21 00:38:33 -0400

    remove production from knexfile

--- knexfile.js
+++ knexfile.js
@@ -8,15 +8,5 @@ export default {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     charset: "utf8",
-  },
-  production: {
-    client: "mysql2",
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-  },
-  
+  },  
 }
