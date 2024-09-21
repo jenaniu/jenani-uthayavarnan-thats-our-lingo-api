b394c06f06169e1a55896b34f817d77da814f801
Jenani Uthayavarnan <jenaniu@hotmail.com>
2024-09-20 21:21:14 -0400

    update knex file

--- knexfile.js
+++ knexfile.js
@@ -9,16 +9,14 @@ export default {
     password: process.env.DB_PASSWORD,
     charset: "utf8",
   },
-  // production: {
-  //   client: "mysql2",
-  //   connection: process.env.JAWSDB_URL,
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations",
-  //   }
-  // }
+  production: {
+    client: "mysql2",
+    connection: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    }
+  }
 //  vercel
 }
