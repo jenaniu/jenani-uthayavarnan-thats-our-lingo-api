504adb3bcd962afb9506406df957f44b31bdc449
Jenani Uthayavarnan <jenaniu@hotmail.com>
2024-09-20 21:32:57 -0400

    testing vercel

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-import "dotenv/config";
+// import "dotenv/config";
+// import express from "express";
+// import cors from 'cors';
+// import knex from "knex";
+
+// const app = express();
+// const { CORS_ORIGIN } = process.env;
+
+// app.use(cors({ origin: CORS_ORIGIN }));
+// app.use(express.json());
+// app.use(express.static('public'));
+
+
+// const PORT = process.env.PORT || 5050;
+
+// import vocabRoutes from "./routes/vocab-routes.js";
+// import grammarRoutes from "./routes/grammar-routes.js";
+
+// app.get('/', (req, res) => {
+//   res.send("That's Our Lingo API!")
+// }); 
+
+// app.use("/vocabulary", vocabRoutes);
+
+// app.use("/grammar", grammarRoutes);
+
+// app.listen(PORT, () => {
+//   console.log(`running at http://localhost:${PORT}`);
+// });
+
 import express from "express";
-import cors from 'cors';
-import knex from "knex";
-
 const app = express();
-const { CORS_ORIGIN } = process.env;
-
-app.use(cors({ origin: CORS_ORIGIN }));
-app.use(express.json());
-app.use(express.static('public'));
-
-
 const PORT = process.env.PORT || 5050;
 
-import vocabRoutes from "./routes/vocab-routes.js";
-import grammarRoutes from "./routes/grammar-routes.js";
-
-// all users routes
 app.get('/', (req, res) => {
-  res.send("That's Our Lingo API!")
-}); 
-
-app.use("/vocabulary", vocabRoutes);
-
-app.use("/grammar", grammarRoutes);
+  res.send("That's Our Lingo API!");
+});
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
