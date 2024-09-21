788df4263b52e3bfb0df36e74f11378878f35f36
Jenani Uthayavarnan <jenaniu@hotmail.com>
2024-09-21 00:21:50 -0400

    add production section in knexfile

--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,14 @@ export default {
     password: process.env.DB_PASSWORD,
     charset: "utf8",
   },
+  production: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+  },
+  
 }
