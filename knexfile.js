9033fb2bf337fd5f0f956e6b384a2366558f55f0
Jenani Uthayavarnan <jenaniu@hotmail.com>
2024-09-20 22:21:20 -0400

    add production

--- knexfile.js
+++ knexfile.js
@@ -9,14 +9,11 @@ export default {
     password: process.env.DB_PASSWORD,
     charset: "utf8",
   },
-  // production: {
-  //   client: "mysql2",
-  //   connection: {
-  //   host: process.env.DB_HOST,
-  //   database: process.env.DB_NAME,
-  //   user: process.env.DB_USER,
-  //   password: process.env.DB_PASSWORD,
-  //   }
-  // }
+  production: {
+    client: "mysql2",
+    connection: {
+      connection: process.env.JAWSDB_URL,
+    }
+  }
 //  vercel
 }
